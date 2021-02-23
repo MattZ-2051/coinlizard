d74187982316fc5216564a83aebeca7be1b22896
Matt Zamora <mzamora2899@gmail.com>
2021-02-23 12:45:17 -0700

    add coin-gecko api and render data returned

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,65 +1,54 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import CoinGecko from "coingecko-api";
 
-export default function Home() {
+// instantiate coin gecko client
+const coinGeckoClient = new CoinGecko();
+
+export default function Home(props) {
+  const { data } = props.res;
+  console.log(data);
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Coin Lizard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <h1>Coin Lizard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>24Hr Change</th>
+            <th>Price</th>
+            <th>Market Cap</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((coin) => {
+            return (
+              <tr key={coin.id}>
+                <td>{coin.symbol.toUpperCase()}</td>
+                <td>{coin.price_change_percentage_24h}</td>
+                <td>{coin.current_price}</td>
+                <td>{coin.market_cap}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  const params = {
+    order: CoinGecko.ORDER.MARKET_CAP_DESC,
+  };
+  const res = await coinGeckoClient.coins.markets({ params });
+  return {
+    props: {
+      res,
+    },
+  };
 }
