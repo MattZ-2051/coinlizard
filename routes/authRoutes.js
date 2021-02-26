016f861bbc539c3512478611a602834ac3a4f22f
Ben Pu <benpu@Bens-MacBook-Pro.local>
2021-02-26 14:17:34 -0500

    re-added passport and auth routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+
+module.exports = (app) => { 
+    // when using auth, new GoogleStrategy() uses string of 'google' hence why <passport.authenticate('google'...)> below
+    app.get('/auth/google', passport.authenticate('google', { // brings up google log-in and authenticates user -> passport is defined in ./services/passport.js
+        
+        // scope specfies what we want access to. In this case: profile and email.
+        scope: ['profile', 'email'] //these strings are not made up - google has a list of strings
+    }));
+
+    app.get('/auth/google/callback', passport.authenticate('google'),
+    (req, res) => {
+        res.redirect('/')
+    });
+
+    app.get('/api/logout', (req, res) => { // this will logout user
+        req.logout();
+        
+        res.redirect('/')
+    });
+
+    app.get('/api/current_user', (req, res) => { // this will return current user
+        res.send(req.user);
+    });
+
+    app.get('/api/test', (req, res) => {
+        res.send({
+            hello: 'Hello'
+        });
+    }); 
+}
