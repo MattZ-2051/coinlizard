ae69fa157b1bacdc65b02e71a1194d67e5a13d18
Matt Zamora <mzamora2899@gmail.com>
2021-02-28 11:28:08 -0700

    add more data to coin profile

--- client/src/components/CoinProfile.js
+++ client/src/components/CoinProfile.js
@@ -16,23 +16,50 @@ export default function CoinProfile() {
   console.log("data", data);
   return (
     <>
-      <div>
-        <a href="/">Home Page</a>
-        <img src={data.image.large} alt="" />
-        <h1>
-          {data.id}({data.symbol})
-        </h1>
-        <p>Symbol: {data.symbol}</p>
-        <p>Market Cap Rank: {data.market_cap_rank}</p>
-        <p>Date Created: {data.genesis_date}</p>
-        <p>Developer Score: {data.developer_score}</p>
-        <p>Community Score: {data.developer_score}</p>
-        <p>Liquidity Score: {data.liquidity_score}</p>
-        <p>Total Supply: {data.total_supply}</p>
-        <p>Max Supply: {data.max_supply}</p>
-        <p>Circulating Supply: {data.circulating_supply}</p>
-        {data.description && <p>{data.description.en}</p>}
+      <a href="/">Home Page</a>
+
+      <div style={{ display: "flex" }}>
+        <div>
+          <img src={data.image.large} alt="" />
+          <h1>
+            {data.id}({data.symbol})
+          </h1>
+        </div>
+        <div>
+          <h5>Market Cap Rank: {data.market_cap_rank}</h5>
+          <h5>Date Created: {data.genesis_date}</h5>
+          <h5>Total Supply: {data.total_supply}</h5>
+          <h5>Max Supply: {data.max_supply}</h5>
+          <h5>Circulating Supply: {data.circulating_supply}</h5>
+        </div>
+        {/* Developer stats from coin gecko */}
+        <div>
+          <h3>Developer Data</h3>
+          <h5>Forks: {data.developer_data.forks}</h5>
+          <h5>Stars: {data.developer_data.stars}</h5>
+          <h5>Subscribers: {data.developer_data.subscribers}</h5>
+          <h5>Total Issues: {data.developer_data.total_issues}</h5>
+          <h5>Closed Issues: {data.developer_data.closed_issues}</h5>
+          <h5>Total Issues: {data.developer_data.total_issues}</h5>
+          <h5>
+            Pull Request Merged: {data.developer_data.pull_requests_merged}
+          </h5>
+          <h5>
+            Commit Count 4-weeks: {data.developer_data.commit_count_4_weeks}
+          </h5>
+        </div>
+        <div>
+          <h3>Community Data</h3>
+          <h5>Facebook Likes: {data.community_data.facebook_likes}</h5>
+          <h5>Twitter Followers: {data.community_data.twitter_followers}</h5>
+          <h5>
+            Reddit average posts 48h:{" "}
+            {data.community_data.reddit_average_posts_48h}
+          </h5>
+        </div>
       </div>
+
+      {data.description && <p>{data.description.en}</p>}
     </>
   );
 }
