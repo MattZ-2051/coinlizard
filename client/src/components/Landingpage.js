b5de517ecd650948c6fc3888db98fd86c91aeff3
Ben Pu <benpu@Bens-MacBook-Pro.local>
2021-03-22 13:31:22 -0400

    styled landing

--- client/src/components/Landingpage.js
+++ client/src/components/Landingpage.js
@@ -1,24 +1,45 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import lizard from '../images/lizard.png';
 import coin from '../images/coin.png';
 import google from '../images/google.png';
 import '../styles/landingpage.css';
 
 const Landingpage = () => {
+    const history = useHistory();
+
+    const toHome = () => {
+        history.push('/home')
+    }
+
+    const loginWithGoogle = () => {
+        history.push('/auth/google')
+    }
+
     return (
         <div className='landing-main'>
             <div className='landing-col-1'>
 
             </div>
             <div className='landing-col-2'>
-                <a href="/home" className="landing-button-coin">
+                <div className="landing-button-coin">
                     <img style={{height: '50px'}}src={coin} />
-                </a>
-                <h2>Enter site</h2>
-                <a href="/home" className="landing-button-google">
+                </div>
+                <div className='landing-button-1'>
+                    <button className='btn' onClick={toHome}>
+                        Enter site
+                    </button>
+                </div>
+                
+                <div className="landing-button-google">
                     <img style={{height: '50px'}}src={google} />
-                </a>
-                <h2>Login with Google</h2>
+                </div>
+                <div className='landing-button-2'>
+                    <a className='btn' href='/auth/google'>
+                        Login with Google
+                    </a>
+                </div>
+                    
             </div>
             <div className='landing-col-3'>
                 <div className='image-div'>
