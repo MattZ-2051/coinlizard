1f5090e0a25e5d6739c3ac52ea7d987a33087b84
Ben Pu <bpu991@gmail.com>
2021-05-03 13:11:59 -0400

    heroku fixes

--- app.js
+++ app.js
@@ -33,6 +33,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/favoritesRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 const PORT = process.env.PORT || 5000;
 // app.listen(PORT);
 app.listen(PORT, () => {
